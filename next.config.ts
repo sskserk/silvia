6531baba81f6e86a47b7e47ae14e18294a95a3f5
sskserk <sskserk@gmail.com>
2026-01-23 15:12:29 +0100

    buildid

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH ?? null;
+  },
 };
 
 export default nextConfig;
