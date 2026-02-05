6cf83b67fed800edb9c606fae3063605cb023b73
sskserk <sskserk@gmail.com>
2026-02-05 14:40:14 +0100

    headers

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
   generateBuildId: async () => {
     // This could be anything, using the latest git hash
     return process.env.GIT_HASH ?? null;
   },
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ]
+  }
 };
 
 export default nextConfig;
